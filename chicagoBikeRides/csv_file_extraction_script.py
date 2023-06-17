45dfb50658e665b0282b9ae9f228530ff5dc56cd
ArnaudDuhamel <113102976+ArnaudDuhamel@users.noreply.github.com>
2023-06-17 21:09:48 +0200

    Add files via upload

---
+++ chicagoBikeRides/csv_file_extraction_script.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+# set the path to the parent directory where the folders are located
+parent_dir = "."
+
+# loop through each folder in the parent directory
+for folder_name in os.listdir(parent_dir):
+
+    # create the path for each folder in the parent folder
+    folder_path = os.path.join(parent_dir, folder_name)
+    
+    # check if the item is a directory, if it is not a folder, it skips it
+    if os.path.isdir(folder_path):
+
+        # loop through each file in the folder
+        for file_name in os.listdir(folder_path):
+
+            # create the path of the element in the subfolder
+            file_path = os.path.join(folder_path, file_name)
+
+            # if the element in the subfolder is another folder, the folder is deleted
+            if os.path.isdir(file_path):
+                 shutil.rmtree(file_path)
+            else:
+                # otherwise, if it is a file, the file is moved to the parent directory
+                shutil.move(file_path, os.path.join(parent_dir, file_name))
+                
+            # delete the now-empty subfolder
+        shutil.rmtree(folder_path)

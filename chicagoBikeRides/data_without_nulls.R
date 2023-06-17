e4bbb426d0aff6e20a5578deb700b997865e9b77
ArnaudDuhamel <113102976+ArnaudDuhamel@users.noreply.github.com>
2023-06-17 22:28:50 +0200

    Add files via upload

--- chicagoBikeRides/data_without_nulls.R
+++ chicagoBikeRides/data_without_nulls.R
@@ -1,44 +1,22 @@
+# load the full dataset into a dataframe
 data <- read.csv("merged_data.csv")
+
+# show the head of the dataframe to verify that the read was successful
 head(data)
 
-distinct_bike_type <- unique(data$rideable_type)
-print(distinct_bike_type)
-
-distinct_member_type <- unique(data$member_casual)
-print(distinct_member_type)
-
-distinct_start_station_id <- unique(data$start_station_id)
-print(distinct_start_station_id)
-
-write.csv(distinct_start_station_id, "start_station_ids.csv", row.names = FALSE)
-
-distinct_start_station_id <- unique(data$start_station_id)
-print(distinct_start_station_id)
-
+# remove entries that have null values
+# this function turned out to be insufficient and did not remove
+# all of the rows with empty attribute values
 data_without_na <- na.omit(data)
 
-write.csv(data_without_na, "data_without_nulls.csv", row.names = TRUE)
 
-distinct_start_station_name <- unique(data_without_na$start_station_name)
-print(distinct_start_station_name)
-
-write.csv(distinct_start_station_name, "start_station_names.csv", row.names = FALSE)
-
+# removing rows where either the start or end station value was
+# an empty string completely eliminated rows with null values
 data_without_na <- data_without_na[data_without_na$end_station_name != "", ]
 data_without_na <- data_without_na[data_without_na$start_station_name != "", ]
 
+# print the number of rows of the datafram for information
 print(nrow(data_without_na))
-write.csv(data_without_na, "data_without_nulls.csv", row.names = TRUE)
 
-distinct_start_station_name <- unique(data_without_na$start_station_name)
-write.csv(distinct_start_station_name, "start_station_names.csv", row.names = FALSE)
-
-distinct_end_station_name <- unique(data_without_na$end_station_name)
-write.csv(distinct_end_station_name, "end_station_names.csv", row.names = FALSE)
-
-distinct_start_lat <- unique(data_without_na$start_lat)
-print(distinct_start_lat)
-
-data_without_na_chars <- apply(data_without_na, 2, as.character)
-
-write.csv(data_without_na_chars, "data_without_nulls.csv", row.names = TRUE)
+# write the filtered and cleaned dataframe to a csv file
+write.csv(data_without_na, "data_without_nulls.csv", row.names = FALSE)
